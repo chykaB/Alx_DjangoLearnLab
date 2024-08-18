19c95a4807719a1afa00872eaebf3e6d0ae40651
chykaB <jodierhees@gmail.com>
2024-08-18 23:04:35 +0100

    Added security settings

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, ExampleForm
 from django.db.models import Q
 from django.db import transaction
 from django.views.decorators.http import require_http_methods
