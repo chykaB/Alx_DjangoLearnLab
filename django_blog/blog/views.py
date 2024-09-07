9bb944019214ac2e496604c98439c86aea9240f6
chykaB <jodierhees@gmail.com>
2024-09-07 23:18:28 +0100

    blog project

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+    form = CustomUserCreationForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("login") 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form":form})
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
+
