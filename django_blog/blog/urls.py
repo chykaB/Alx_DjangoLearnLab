0e970029101804f2eafb3603c20a286b7544b16a
chykaB <jodierhees@gmail.com>
2024-09-07 23:20:38 +0100

    blog project

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import register, profile
 
 urlpatterns = [
-    path("login", auth_views.LoginView.as_view(), name="login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
-    path("register", register, name="register"),
+    path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
 ]
