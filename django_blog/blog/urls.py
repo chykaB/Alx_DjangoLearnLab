06aaf93d7fc2efa1eca679711ca8ab0031ab102e
chykaB <jodierhees@gmail.com>
2024-09-10 16:58:43 +0100

    Implemented Crud operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("accounts/profile/", profile, name="profile"),
     path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>", PostDetailView.as_view, name="post-detail" ),
-    path("post/new", PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>", PostUpdateView.as_view(), name="post-update"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDelete.as_view(), name="post-delete"),
 
 ]
