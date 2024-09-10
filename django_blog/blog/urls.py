332be3dcdb91d5eb785ea40b579bfc2cb415b06d
chykaB <jodierhees@gmail.com>
2024-09-10 18:48:39 +0100

    Implemented Crud operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDelete.as_view(), name="post-delete"),
 
     path("posts/<int:post_id>,comments/new/", CommentCreateView, name="comments-new"),
-    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-edit"),
-    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 
 ]
