f102cc44e5aca95e3b2faa801e577206440160db
chykaB <jodierhees@gmail.com>
2024-10-29 18:19:21 +0100

    Added the Book model to the admin site

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Book
+        list_display = ["title", "author", "publication_year"]
+
+admin.site.register(Book, BookAdmin)
