0e09f9bd0796024fe233710547565363c5f36f11
chykaB <jodierhees@gmail.com>
2024-11-11 12:02:38 +0100

    added a directory

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -7,7 +7,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
