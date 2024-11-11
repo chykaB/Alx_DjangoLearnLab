ca205ca30302cfd70981fff1eb6fa94f9f805383
chykaB <jodierhees@gmail.com>
2024-11-11 11:58:42 +0100

    added a directory

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -6,8 +6,8 @@ author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
 
 # List all books in a library.
-books = Library.objects.get(name=library_name)
-books.all()
+# books = Library.objects.get(name=library_name)
+# books.all()
 
 # Retrieve the librarian for a library
 library_name = "Prempeh library"
